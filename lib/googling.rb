e5a6f3381cb386861f80c35213e352e2158db6bd
Imyuan <smilestar8@hotmail.com>
2014-07-07 16:08:59 +0800

    modified the Googling

--- lib/googling.rb
+++ lib/googling.rb
@@ -1,5 +1,27 @@
 require "googling/version"
 
 module Googling
-  # Your code goes here...
+  def self.check
+    check = `html2text -version` rescue nil
+    if check 
+      puts "Fine, go next step."
+      true
+    else    
+      puts "Please install the html2text."
+      false
+    end 
+  end
+
+  def self.search
+    puts "Enter your search,please."
+    yoursearch = gets.chomp()
+    url = "curl -A Mozilla http://www.google.com/search?q=#{yoursearch} |html2text -width 70"
+    answer = `curl #{url}`
+    puts "#{answer}"
+  end
 end
+
+if Googling.check
+  puts Googling.search
+end
+ 
