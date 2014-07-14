1eff72aa630011f9f204f8fc57b0bf90acaf0991
Imyuan <smilestar8@hotmail.com>
2014-07-14 18:47:59 +0800

    extract executable to bin/

--- lib/googling.rb
+++ lib/googling.rb
@@ -7,14 +7,3 @@ module Googling
     Googling::Search.new(keywords).execute
   end
 end 
-
-puts "please enter the KEY WORDS."
-keywords = gets.chomp('')  
-
-results = Googling.search(keywords)
-results.each do |result|
-  puts ">>>> "
-  puts result.title
-  puts result.link
-  puts result.description
-end
